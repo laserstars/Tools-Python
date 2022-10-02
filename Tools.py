0a358d990f3882907cb3652d53b937ceccb3a620
BuildTools <unconfigured@null.spigotmc.org>
2022-10-02 12:43:52 +0200

    1.2 -Ajout de Option Jeux -Ajout du jeu Pierre Feuille Ciseaux

--- Tools.py
+++ Tools.py
@@ -1,93 +1,163 @@
 import random
+from random import randint
+Menu = 1
 
-print("0 -- Calculatrice")
-print("1 -- Générateur de mot de passe")
-option = int(input(""))
+while Menu == 1:
 
-fin1 = 1
-fin2 = 1
+    print("0 -- Calculatrice")
+    print("1 -- Générateur de mot de passe")
+    print("2 -- Jeux")
+    option = int(input(""))
 
-print("")
-
-if option == 0:
-    while fin1 == 1:
-
-        chiffre1 = int(input("Choisis ton premier nombre "))
-
-        print("")
-
-        print("0 -- Multiplier par ... ")
-        print("1 -- Diviser par ... ")
-        print("2 -- Additionner par ... ")
-        print("3 -- Soustraire par ... ")
+    if (option > 2 or option < 0):
+        print("\nOpération impossible, choisissé un bon chiffre.\n")
+    else:
+        fin_calculatrice = 1
+        fin_password = 1
+        fin_jeu = 1
 
         print("")
 
-        symbole = int(input("Choisir une opération "))
+        if option == 0:
+            while fin_calculatrice == 1:
 
-        print("")
+                chiffre1 = int(input("Choisis ton premier nombre "))
 
-        chiffre2 = int(input("Choisis ton deuxième nombre "))
+                print("")
 
-        print("")
+                print("0 -- Multiplier par ... ")
+                print("1 -- Diviser par ... ")
+                print("2 -- Additionner par ... ")
+                print("3 -- Soustraire par ... ")
+
+                print("")
+
+                operation = int(input("Choisir une opération "))
+
+                print("")
+
+                chiffre2 = int(input("Choisis ton deuxième nombre "))
+
+                print("")
+
+                if operation == 0:
+                    multiplication = chiffre1 * chiffre2
+                    print("Le résultat est ", multiplication)
+
+                elif operation == 1:
+                    if chiffre2 == 0:
+                        print("Division par zéro impossible !")
+                    else:
+                        division = chiffre1 / chiffre2
+                        print("Le résultat est ", division)
+
+                elif operation == 2:
+                    addition = chiffre1 + chiffre2
+                    print("Le résultat est ", addition)
+
+                elif operation == 3:
+                    soustration = chiffre1 - chiffre2
+                    print("Le résultat est ", soustration)
+
+                print("")
+
+                print("0 -- Retour au Menu")
+                print("1 -- Nouvelle opération")
+
+                print("")
+
+                print("Que faire ?")
+
+                fin_calculatrice = int(input(""))
+
+                print("")
+
+        elif option == 1:
+            while fin_password == 1:
+                lower_case = "abcdefghijklmnopqrstuvwxyz"
+                upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+                number_password = "0123456789"
+                symbols_password = "\/#@%&*$"
+
+                Use_for = lower_case + upper_case + number_password + symbols_password
+                lenght_for_pass = int(input("Quelle taille pour le mot de passe ? "))
+
+                print("")
+
+                password = "".join(random.sample(Use_for, lenght_for_pass))
+                print("Le nouveau mot de passe est " + password)
+
+                print("")
+
+                print("0 -- Retour au Menu")
+                print("1 -- Nouveau mot de passe")
+
+                print("")
+
+                print("Que faire ?")
+
+                fin_password = int(input(""))
+
+                print("")
+
+        elif option == 2:
+            while fin_jeu == 1:
+                print("0 -- Pierre, Feuille, Ciseaux")
 
 
-        if symbole == 0:
-            multiplication = chiffre1 * chiffre2
-            print("Le résultat est ", multiplication)
+                option_jeu = int(input(""))
 
-        elif symbole == 1:
-            if chiffre2 == 0:
-                print("Division par zéro impossible !")
-            else:
-                division = chiffre1 / chiffre2
-                print("Le résultat est ", division)
+                if option_jeu == 0:
+                    fin_jeu_PSF = 1
 
-        elif symbole == 2:
-            addition = chiffre1 + chiffre2
-            print("Le résultat est ", addition)
+                    while fin_jeu_PSF == 1:
+                        option_jeu = ['pierre', 'feuille', 'ciseaux']
+                        chiffre_option_jeu = randint(0, 2)
+                        choix_ordi = option_jeu[chiffre_option_jeu]
 
-        elif symbole == 3:
-            soustration = chiffre1 - chiffre2
-            print("Le résultat est ", soustration)
+                        choix_joueur = input("\npierre, feuille, ciseaux. Taper Fin pour quitter.\n")
 
-        print("")
+                        if (
+                                choix_joueur == 'pierre' or choix_joueur == 'Pierre' or choix_joueur == 'PIERRE' or choix_joueur == 'P' or choix_joueur == 'p'):
 
-        print("0 -- Quitter")
-        print("1 -- Nouvelle opération")
+                            choix_joueur = 'pierre'
+                            print('\nVous : ', choix_joueur, ' VS ', choix_ordi, ' : Ordinateur\n')
 
-        print("")
+                        elif (
+                                choix_joueur == 'feuille' or choix_joueur == 'Feuille' or choix_joueur == 'FEUILLE' or choix_joueur == 'F' or choix_joueur == 'f'):
 
-        print("Que faire ?")
+                            choix_joueur = 'feuille'
+                            print('\nVous : ', choix_joueur, ' VS ', choix_ordi, ' : Ordinateur\n')
 
-        fin1 = int(input(""))
+                        elif (
+                                choix_joueur == 'ciseaux' or choix_joueur == 'Ciseaux' or choix_joueur == 'CISEAUX' or choix_joueur == 'C' or choix_joueur == 'c' or choix_joueur == 'ciseau' or choix_joueur == 'Ciseau' or choix_joueur == 'CISEAU'):
 
-        print("")
+                            choix_joueur = 'ciseaux'
+                            print('\nVous : ', choix_joueur, ' VS ', choix_ordi, ' : Ordinateur\n')
 
-elif option == 1:
-    while fin2 == 1:
-        lower_case = "abcdefghijklmnopqrstuvwxyz"
-        upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-        number = "0123456789"
-        symbols = "\/#@%&*$"
+                        if choix_joueur == choix_ordi:
+                            print('Egalité!')
 
-        Use_for = lower_case + upper_case + number + symbols
-        lenght_for_pass = int(input("Quelle taille pour le mot de passe ? "))
+                        elif choix_joueur == 'pierre':
+                            if choix_ordi == 'feuille':
+                                print('Perdu! La feuille recouvre la pierre')
+                            else:
+                                print('Gagné! La pierre écrase les ciseaux')
+                        elif choix_joueur == 'feuille':
+                            if choix_ordi == 'pierre':
+                                print('Gagné! La feuille recouvre la pierre.')
+                            else:
+                                print('Perdu! Les ciseaux coupent la feuille.')
+                        elif choix_joueur == 'ciseaux':
+                            if choix_ordi == 'feuille':
+                                print('Gagné! Les ciseaux coupent la feuille.')
+                            else:
+                                print('Perdu! Les ciseaux se font écraser par la pierre.')
 
-        print("")
+                        elif (choix_joueur == 'fin' or choix_joueur == 'Fin' or choix_joueur == 'FIN'):
+                            fin_jeu_PSF = 0
 
-        password = "".join(random.sample(Use_for, lenght_for_pass))
-        print("Le nouveau mot de passe est " + password)
+                        else:
+                            print('\nErreur, option invalide.')
 
-        print("")
-
-        print("0 -- Quitter")
-        print("1 -- Nouveau mot de passe")
-
-        print("")
-
-        print("Que faire ?")
-
-        fin2 = int(input(""))
-
-        print("")
+                        fin_jeu_PSF1 = int(input('\n0 -- Choix jeux\n1-- Rejouer\n'))
